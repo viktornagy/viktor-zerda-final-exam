57516e7f1089f1fe208213cd91531457e9734907
viktornagy <vnagy2000@gmail.com>
2017-02-17 10:29:23 +0100

    Solution of exercise 2

--- src/main/java/FinalExam.java
+++ src/main/java/FinalExam.java
@@ -8,18 +8,16 @@ import java.util.List;
 public class FinalExam {
 
     static List<Integer> listOfNumbers = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
-    static ArrayList<Integer> howManyOddNumbers = new ArrayList<>();
+    static int howManyOddNumbers=0;
     static int sumOfOddNumbers=0;
-    static int itemOfNumbers=0;
     public static void main(String[] args) {
 
         for (int number: listOfNumbers) {
             if (number % 2 == 0) {
-                howManyOddNumbers.add(number);
+                howManyOddNumbers++;
                 sumOfOddNumbers+=number;
             }
         }
-        itemOfNumbers=howManyOddNumbers.size();
-        System.out.println(sumOfOddNumbers/itemOfNumbers);
+        System.out.println(sumOfOddNumbers/howManyOddNumbers);
     }
 }
